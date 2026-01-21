431432594fc4576614b116b32ba556322a72f6cb
Zenithi77 <tamiraatami777@gmail.com>
2026-01-21 14:52:29 +0800

    chore: Remove admin/brands and admin/categories pages

--- selly-fashion-app/src/app/admin/page.tsx
+++ selly-fashion-app/src/app/admin/page.tsx
@@ -71,27 +71,6 @@ export default function AdminDashboard() {
       color: 'green'
     },
     { 
-      name: 'Категори', 
-      href: '/admin/categories', 
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
-          <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6Z" />
-        </svg>
-      ),
-      color: 'purple'
-    },
-    { 
-      name: 'Брэнд', 
-      href: '/admin/brands', 
-      icon: (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
-        </svg>
-      ),
-      color: 'yellow'
-    },
-    { 
       name: 'Хямдрал', 
       href: '/admin/sale', 
       icon: (
